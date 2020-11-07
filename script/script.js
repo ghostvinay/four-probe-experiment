da82e6761a5e31827330df4914869b80e17e5a4a
Shibu778 <shibumeher6@gmail.com>
2020-11-07 21:08:46 +0530

    First Commit

---
+++ script/script.js
@@ -0,0 +1,19 @@
+//Function for aim of the Experiment
+function aim() {
+  document.getElementById('environment').innerHTML = document.getElementById('aim_env').innerHTML;
+}
+
+//Function for intro of the experiment
+function intro() {
+  document.getElementById('environment').innerHTML = document.getElementById('intro_env').innerHTML;
+}
+
+//Function for demo1
+function demo1() {
+  document.getElementById('environment').innerHTML = document.getElementById('demo1_env').innerHTML;
+}
+
+//Function for demo2
+function demo2() {
+  document.getElementById('environment').innerHTML = document.getElementById('demo2_env').innerHTML;
+}
